4e698658f6424a049df466c1a6b2ca9ea8bf2ea3
nora <vargheeseaaronv@gmail.com>
2025-02-23 19:45:05 +0530

    backend endpoints added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: "http://localhost:5000"
+    // apiUrl: "http://localhost:5000"
+    apiUrl: "https://mean-backend-test1.onrender.com"
   };
   
